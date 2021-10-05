17280135ce9b69dfb687e155d9a093361e99da42
Vaishnavi Bholane <87429198+vaishnavi-bholane@users.noreply.github.com>
2021-10-05 18:38:45 +0530

    Add files via upload

---
+++ Task-02/code.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+namespace first
+{
+	int add(int a, int b)
+	{
+		return a+b;
+	}
+
+}
+namespace second
+{
+	float add(float a, float b)
+	{
+		return a+b;
+	}
+
+}
+using namespace first;
+using namespace second;
+
+int main()
+{
+	int a = add(4,2.1);
+	cout << "Sum = "<<a<<endl;
+	return 0;
+}
+
